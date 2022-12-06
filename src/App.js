30b4102ec1ab576a0823a5a48d93bcbf48f151d6
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-12-06 09:42:41 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Questify #1</h1>
+        <p>Paweł</p>
+        <p>Patrycja</p>
+        <p>Jakub</p>
+        <p>Stachu</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

c53c4f7bf0fe2556ddd83d1a41a061ed9241ec49
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-12-09 09:11:22 +0100

    landing-page-done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import SideImages from "../../components/SideImages/SideImages";
 import { ReactComponent as Rectangle } from "../../images/Rectangle.svg";
 import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h3 className="title">Questify</h3>
-      <p className="description">
-        Questify will turn your life into a thrilling game full of amazing
-        quests and exciting challenges.
-      </p>
-      <p className="login-info">Choose your name to sign up or log in</p>
-      <LoginForm />
-      <SideImages />
-      <Rectangle className="rectangle" />
+    <div className="home">
+      <div className="container">
+        <h3 className="title">Questify</h3>
+        <p className="description">
+          Questify will turn your life into a thrilling game full of amazing
+          quests and exciting challenges.
+        </p>
+        <p className="login-info">Choose your name to sign up or log in</p>
+        <LoginForm />
+        <Rectangle className="rectangle" />
+      </div>
     </div>
   );
 };

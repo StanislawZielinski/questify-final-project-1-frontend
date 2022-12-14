7bbd7081fc9c78e7e1453c3958330a1be71b9c72
PawelPalasinski <pawel.palasinski@gmail.com>
2022-12-14 21:18:26 +0100

    form

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { QuestListsContainer } from "../../components/QuestListsContainer/QuestListsContainer";
 
 function Dashboard() {
-  return <div>DASHBOARD!!!</div>;
+  <>
+    return <div>DASHBOARD!!!</div>;
+    <QuestListsContainer />
+  </>;
 }
 
 export default Dashboard;

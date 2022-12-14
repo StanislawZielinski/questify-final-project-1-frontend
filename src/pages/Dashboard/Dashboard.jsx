d2b4940dc6856c91a4afcdc6893f465085e0c918
PawelPalasinski <pawel.palasinski@gmail.com>
2022-12-14 22:19:17 +0100

    poprawki

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { QuestListsContainer } from "../../components/QuestListsContainer/QuestListsContainer";
 
 function Dashboard() {
-  <>
-    return <div>DASHBOARD!!!</div>;
-    <QuestListsContainer />
-  </>;
+  return (
+    <>
+      <div>DASHBOARD!!!</div>
+      <QuestListsContainer />
+    </>
+  );
 }
 
 export default Dashboard;

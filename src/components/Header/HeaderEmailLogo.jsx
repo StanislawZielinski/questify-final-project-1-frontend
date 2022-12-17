85b0be5e17c55a25caa6b7bfd07b7a74dad7f8dc
PawelPalasinski <pawel.palasinski@gmail.com>
2022-12-17 18:06:27 +0100

    poprawka w HeaderEmailLogo

--- src/components/Header/HeaderEmailLogo.jsx
+++ src/components/Header/HeaderEmailLogo.jsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import { selectUserName } from "../../redux/auth/selectors";
 
 const HeaderEmailLogo = () => {
-  const email = useSelector(selectUserName);
-  const firstLetterOfEmail = email.charAt(0).toUpperCase();
-
+  const { email } = useSelector(selectUserName);
+  const firstLetterOfEmail = email === null ? '?' : email.charAt(0).toUpperCase();
+  console.log(firstLetterOfEmail);
   return (
     <>
       <p className="header-nav-email">{firstLetterOfEmail}</p>
